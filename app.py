220a282f00c869121f9e4617365959f66ffd4c80
Jooonyub <59817144+Jooonyub@users.noreply.github.com>
2021-03-03 16:33:18 +0900

    뼈대 올려드립니다. 기본 템플릿, 로그인 템플릿, 지도와리스팅 템플릿, 작성템플릿 네가지 html이에요

--- app.py
+++ app.py
@@ -1,63 +1,53 @@
 from pymongo import MongoClient
 import jwt
 import datetime
-import hashlib
+import hashlib  #hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
 
-
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
-
 SECRET_KEY = 'SPARTA'
 
 #일단은 로컬로 되는지 파악한 후에 AWS로 연결하기
 client = MongoClient('localhost', 27017)
 #client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
-db = client.dbsparta_plus_week4
+db = client.members
 
 #기본화면
 @app.route('/')
 def home():
+    '''
+    쿠키에 로그인 정보가 있으면 payload 작성 후 index.html 렌더링
+    정보가 없으면 login창으로 redirect(알맞은 메시지 작성)
+    '''
+    #발급한 jwt토큰(여기에 토큰이 답겨져서 서버로 온다)
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
-        return render_template('index.html')
+        user_info = db.user.find_one({"id": payload["id"]})
+        return render_template('index.html', user=user_info["username"])
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-#로그인화면
+#로그인 창 API
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
-#유저별 포스트 나열
-@app.route('/user/<username>')
-def user(username):
-    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
-
-        user_info = db.users.find_one({"username": username}, {"_id": False})
-        return render_template('user.html', user_info=user_info, status=status)
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
-
-#로그인화면
+#로그인기능 API
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
     # 로그인
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
 
+    #입력받은 비밀번호에 대한 암호화 진행(hashlib.sha256())
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     result = db.users.find_one({'username': username_receive, 'password': pw_hash})
 
@@ -66,6 +56,7 @@ def sign_in():
          'id': username_receive,
          'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
+        #페이로드에 대한 암호화(jwt)
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
 
         return jsonify({'result': 'success', 'token': token})
@@ -73,7 +64,7 @@ def sign_in():
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
-#회원가입 저장
+#회원가입 API
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
@@ -82,22 +73,23 @@ def sign_up():
     doc = {
         "username": username_receive,                               # 아이디
         "password": password_hash,                                  # 비밀번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-        "profile_pic": "",                                          # 프로필 사진 파일 이름
-        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
-        "profile_info": ""                                          # 프로필 한 마디
+        #"profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        #"profile_pic": "",                                          # 프로필 사진 파일 이름
+        #"profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
+        #"profile_info": ""                                          # 프로필 한 마디
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
 
-#회원가입 중복확인
+#회원가입시 중복확인 API
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
-#프로필 업데이트
+'''
+#프로필 업데이트(일단 x. 프로필이미지 기능 사용하지 않기때)
 @app.route('/update_profile', methods=['POST'])
 def save_img():
     token_receive = request.cookies.get('mytoken')
@@ -107,8 +99,9 @@ def save_img():
         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
-
-#게시글 작성
+'''
+'''
+#포스트 기능(form:장소이름, 코멘트)
 @app.route('/posting', methods=['POST'])
 def posting():
     token_receive = request.cookies.get('mytoken')
@@ -119,7 +112,7 @@ def posting():
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
-#게시글 불러오기->
+#포스트 목록 가져오기(장소이름, 코멘트, 장소 geodata, 사용자 ID)
 @app.route("/get_posts", methods=['GET'])
 def get_posts():
     token_receive = request.cookies.get('mytoken')
@@ -129,8 +122,11 @@ def get_posts():
         return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다."})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
+'''
 
-#좋아요 업데이트
+
+'''
+#좋아요 API(일단 보류) 
 @app.route('/update_like', methods=['POST'])
 def update_like():
     token_receive = request.cookies.get('mytoken')
@@ -140,7 +136,7 @@ def update_like():
         return jsonify({"result": "success", 'msg': 'updated'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
-
+'''
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
