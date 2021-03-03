024d8c17c00458e8a1570143c97cfde6761d108d
Jooonyub <59817144+Jooonyub@users.noreply.github.com>
2021-03-03 13:36:30 +0900

    second commit

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
+#일단은 로컬로 되는지 파악한 후에 AWS로 연결하기
+client = MongoClient('localhost', 27017)
+#client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
 db = client.dbsparta_plus_week4
 
 #기본화면
